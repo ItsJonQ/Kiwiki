af7b3c78b65cbf7f85a7e2ca13b3fadc1aa43fff
ItsJonQ <itsjonq@gmail.com>
2014-12-07 16:31:46 -0500

    Added get/set post thumbnail related methods to base model

--- app/models/base.php
+++ app/models/base.php
@@ -12,6 +12,51 @@ class QiBaseModel {
     return $permalink;
   }
 
+  public function get_post_thumbnail_meta( $size = null ) {
+    global $post;
+
+    // Return if post_thumbnails is disabled in the config
+    if( !Qi_::$config['post_thumbnails'] ) {
+      return;
+    }
+
+    $post_thumbnail = null;
+    $post_thumbnail_id = get_post_thumbnail_id( $post->ID );
+
+    if( $post_thumbnail_id ) {
+
+      // Setting the image ID
+      $post_thumbnail['id'] = $post_thumbnail_id;
+
+      // Getting the thumbnail meta
+      $post_thumbnail = wp_get_attachment_image_src( $post_thumbnail_id, $size );
+
+      // Image alt text
+      $post_thumbnail_alt = get_post_meta( $post_thumbnail_id, '_wp_attachment_image_alt', true);
+
+      if( !$post_thumbnail_alt ) {
+        $post_thumbnail_alt = $this->title;
+      }
+      // Setting the image alt meta info
+      $post_thumbnail['alt'] = $post_thumbnail_alt;
+    }
+
+    return $post_thumbnail;
+  }
+
+  public function get_post_thumbnail_url( $size = null ) {
+    global $post;
+
+    // Getting the thumbail meta
+    $post_thumbnail = self::get_post_thumbnail_meta( $size );
+
+    if( isset( $post_thumbnail[0] ) ) {
+      $post_thumbnail = $post_thumbnail[0];
+    }
+
+    return $post_thumbnail;
+  }
+
   public function set_permalink( $permalink = "" ) {
     global $post;
 
@@ -27,6 +72,18 @@ class QiBaseModel {
     $this->permalink = $permalink;
 
     return $permalink;
+  }
+
+  public function set_featured_image_url() {
+    $featured_image = self::get_post_thumbnail_url();
+
+    $this->featured_image_url = $featured_image;
+  }
+
+  public function set_post_thumbnail_url() {
+    $post_thumbnail = self::get_post_thumbnail_url( 'thumbnail' );
+
+    $this->post_thumbnail_url = $post_thumbnail;
   }
 
   public function set_title( $title = null ) {
@@ -52,10 +109,16 @@ class QiBaseModel {
   // Initializing the model
   public function __construct() {
 
+    // Initialize
     self::set_model();
 
+    // Base
     self::set_permalink();
     self::set_title();
+
+    // Image
+    self::set_featured_image_url();
+    self::set_post_thumbnail_url();
 
   }
 }
